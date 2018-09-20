6eef297c3abacc76bcaabd58dac9604f93ccd46f
Matt <matthew@beardsley.com.au>
2018-09-20 03:12:59 +0000

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
       
 socket.on('connect', function () {
   console.log('Connected to server')
-  
-  socket.emit('createMessage', {
-    from: 'Matt',
-    text: 'Hey this is matt'
-  })
 });
       
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-
 const socketIO = require('socket.io');
 
 
@@ -14,14 +13,13 @@ var io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('New user connected');
   
-  socket.emit('newMessage', {
-    from: 'mike',
-    text: 'Hey what is going on',
-    createdAt: 123
-  });
-  
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   })
   
   socket.on('disconnect', () => {
