d1c7cc11146bcf90f4325bc727afa5468a0d8ea1
Matt <matthew@beardsley.com.au>
2018-09-26 09:15:45 +0000

    add autoscoll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,20 @@
 var socket = io();
+
+function scrollToBottom() {
+  //Selectors
+  var messages = $('#messages');
+  var newMessage = messages.children('li:last-child')
+  //Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+  
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
       
 socket.on('connect', function () {
   console.log('Connected to server')
@@ -18,6 +34,7 @@ socket.on('newMessage', function (message){
   });
   
   $('#messages').append(html);
+  scrollToBottom();
   
   // // jQuery and $ can be swapped
   // // var li = jQuery('<li></li>');
@@ -38,6 +55,7 @@ socket.on('newLocationMessage', function(message){
   });
   
   $('#messages').append(html);
+  scrollToBottom();
 })
 
 $('#message-form').on('submit', function(e) {
